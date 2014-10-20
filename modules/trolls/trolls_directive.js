b9a632fb2463374465ddf82da2249aa79a6e592d
jilucev <jilucev7@gmail.com>
2014-10-20 11:31:55 -0700

    add troll template

--- modules/trolls/trolls_directive.js
+++ modules/trolls/trolls_directive.js
@@ -2,6 +2,6 @@ angular.module('treasureTrolls')
   .directive('trollsDirective', function () {
     return {
       restrict: 'EA',
-      template: 'Name: {{troll.name}} Hair Color: {{troll.haircolor}}'
+      templateUrl: 'modules/trolls/trolls.html'
     }
   });
